a8cff58ea4ff5dc05edcccd1afb6d4684b2275e9
snidhedinesh <snidhe2016@gmail.com>
2025-03-19 18:19:00 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,335 @@
+<?php include 'includes/header.php' ?>
+
+
+    <div class="top-bar">
+        <div>📞 000-111-222 | ✉ example@example.com</div>
+        <div>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-facebook"></i></a>
+        </div>
+    </div>
+    <div class="nav position-relative">
+        <div class="logo"><img src="bootstrap/images/logo-img.png" alt=""></div>
+        <div class="menu">
+            <a href="#">Home</a>
+            <a href="#">Our Cakes</a>
+            <a href="#">Menu</a>
+            <a href="#">About Us</a>
+            <a href="#">Pages</a>
+            <a href="#">Blog</a>
+            <a href="#">Shop</a>
+            <a href="#">Contact Us</a>
+        </div>
+        <div class="nav-div"><img src="bootstrap/images/nav-img.png" alt=""></div>
+
+    </div>
+    <div class="hero">
+        <button class="slider-btn prev" onclick="prevSlide()">❮</button>
+        <img id="hero-img" class="home-back-img" src="bootstrap/images/home-img.png" alt="Hero Image">
+        <div class="hero-content">
+            <h1 id="hero-title">Freshly baked pastries are the true <br><strong>language of love</strong></h1>
+            <p id="hero-text">This drastic appeal for baked goods promoted baking all throughout Europe and expanded
+                into the eastern parts of Asia.</p>
+            <button class="btn">See the recipe</button>
+        </div>
+        <button class="slider-btn next" onclick="nextSlide()">❯</button>
+    </div>
+    <div class="home-section">
+        <div class="container d-flex">
+            <div class="image-section">
+                <img src="bootstrap/images/back-img.png" alt="Richstack Bakery">
+            </div>
+            <div class="text-section">
+                <h2>Welcome to Richstack</h2>
+                <p><i>Baked goods have been around for thousands of years. The art of baking was developed early during
+                        the Roman Empire.</i></p>
+                <p>It was a highly famous art as Roman citizens loved baked goods and demanded them frequently for
+                    important occasions such as feasts and weddings. Due to the fame and desire that the art of baking
+                    received, around 300 BC, baking was introduced as an occupation and a respectable profession for
+                    Romans.</p>
+                <a href="#" class="home-btn">Know more about us</a>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <section class="featured-cakes position-relative">
+            <button class="slider-btn-card prev-btn" onclick="scrollLeftDest()">❮</button>
+            <button class="slider-btn-card next-btn" onclick="scrollRightDest()">❯</button>
+
+
+            <h2>Our Featured Cakes</h2>
+            <p class="subtitle">Seldolor sit amet consect etur</p>
+            <div class="swiper-wrapper">
+                <div class="card home-card">
+                    <div class="position-relative">
+                        <img src="bootstrap/images/cake-img1.png" alt="Cupcake">
+                        <div class="white-div">
+                            <label for="" class="mx-2">₹1200</label> ₹1200
+                        </div>
+
+                    </div>
+                    <div class="card-body text-center">
+
+                        <h3>Custom Cup Cake</h3>
+                        <button class="cart-btn">Add to cart</button>
+                    </div>
+
+
+                </div>
+                <div class="card home-card">
+                    <div class="position-relative">
+                        <img src="bootstrap/images/cake-img1.png" alt="Cupcake">
+                        <div class="white-div">
+                            <label for="" class="mx-2">₹1200</label> ₹1200
+                        </div>
+
+                    </div>
+                    <div class="card-body text-center">
+
+                        <h3>Custom Cup Cake</h3>
+                        <button class="cart-btn">Add to cart</button>
+                    </div>
+
+
+                </div>
+                <div class="card home-card">
+                    <div class="position-relative">
+                        <img src="bootstrap/images/cake-img1.png" alt="Cupcake">
+                        <div class="white-div">
+                            <label for="" class="mx-2">₹1200</label> ₹1200
+                        </div>
+
+                    </div>
+                    <div class="card-body text-center">
+
+                        <h3>Custom Cup Cake</h3>
+                        <button class="cart-btn">Add to cart</button>
+                    </div>
+
+
+                </div>
+                <div class="card home-card">
+                    <div class="position-relative">
+                        <img src="bootstrap/images/cake-img1.png" alt="Cupcake">
+                        <div class="white-div">
+                            <label for="" class="mx-2">₹1200</label> ₹1200
+                        </div>
+
+                    </div>
+                    <div class="card-body text-center">
+
+                        <h3>Custom Cup Cake</h3>
+                        <button class="cart-btn">Add to cart</button>
+                    </div>
+
+
+                </div>
+                <div class="card home-card">
+                    <div class="position-relative">
+                        <img src="bootstrap/images/cake-img1.png" alt="Cupcake">
+                        <div class="white-div">
+                            <label for="" class="mx-2">₹1200</label> ₹1200
+                        </div>
+
+                    </div>
+                    <div class="card-body text-center">
+
+                        <h3>Custom Cup Cake</h3>
+                        <button class="cart-btn">Add to cart</button>
+                    </div>
+
+
+                </div>
+                <div class="card home-card">
+                    <div class="position-relative">
+                        <img src="bootstrap/images/cake-img1.png" alt="Cupcake">
+                        <div class="white-div">
+                            <label for="" class="mx-2">₹1200</label> ₹1200
+                        </div>
+
+                    </div>
+                    <div class="card-body text-center">
+
+                        <h3>Custom Cup Cake</h3>
+                        <button class="cart-btn">Add to cart</button>
+                    </div>
+
+
+                </div>
+
+            </div>
+
+
+
+        </section>
+
+    </div>
+    <section class="special-div">
+        <div class="position-relative"><img src="bootstrap/images/section-img.png" alt="" width="100%">
+            <div class="inner-cake-div">
+                <div class="row ">
+                    <div class="col-lg-6 com-sm-12">
+                        <div class=""><img src="bootstrap/images/cake-img-absolute.png" alt=""></div>
+
+                    </div>
+                    <div class="col-lg-6 com-sm-12 img-row-div">
+                        <div class="inner-div-data">
+                            <h2>Special Recipe</h2>
+                            <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam
+                                corporis suscipit laboriosam, nisi ut aliquid ex ea commodi equatur
+                                uis autem vel eum</p>
+                            <button class="detail-btn">View Details</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <section class="service-div">
+        <div class="cake-img-double"><img src="bootstrap/images/absolute-img-cake.png" alt=""></div>
+        <h2>Main Services We Provide</h2>
+        <div class="list-div">
+            <div>
+                <img src="bootstrap/images/cake-icon-1.png" alt="">
+                <h3>Celebration Cakes</h3>
+                <p>We offer huge variety of Celebration Cakes.
+                    Check out our collection</p>
+            </div>
+            <div>
+                <img src="bootstrap/images/cake-icon-2.png" alt="">
+                <h3>Birthday Cakes</h3>
+                <p>Our collection for birthday cake is huge.
+                    Volup tate velit esse cillum dolore.</p>
+            </div>
+            <div>
+                <img src="bootstrap/images/cake-icon-3.png" alt="">
+                <h3>Wedding Cakes</h3>
+                <p>Duis aute irure dolor in reprehenderit in
+                    volup tate velit esse cillum dolore</p>
+            </div>
+
+            <div>
+                <img src="bootstrap/images/cake-icon-4.png" alt="">
+                <h3>Birthday Cakes</h3>
+                <p>Our collection for birthday cake is huge.
+                    Volup tate velit esse cillum dolore</p>
+            </div>
+            <div>
+                <img src="bootstrap/images/cake-icon-2.png" alt="">
+                <h3>Birthday Cakes</h3>
+                <p>Our collection for birthday cake is huge.
+                    Volup tate velit esse cillum dolore</p>
+            </div>
+        </div>
+    </section>
+    <div class="menu-container position-relative">
+        <div class="left-img">
+            <img src="bootstrap/images/opacity-img-cake.png" alt="">
+        </div>
+        <div class="right-img">
+            <img src="bootstrap/images/opacity-img-cake-2.png" alt="">
+        </div>
+        <div class="container space-evenly">
+           
+            <div class="menu-content">
+                <h2>Discover Menu</h2>
+                <label>We offer a wide range of Menu items.</label>
+                <div class="row mt-3">
+                <div class="col-6">
+
+                    <div class="menu-item">
+                        <div class="item-name">
+                            <h3>Double Chocolate Pie</h3>
+                            <p>Chocolate pudding, vanilla, fruit raspberry jam milk</p>
+                        </div>
+                        <div class="item-price">₹1000</div>
+                    </div>
+
+                    <div class="menu-item">
+                        <div class="item-name">
+                            <h3>Zabaglione Cake</h3>
+                            <p>Vanilla, fruit raspberry jam milk</p>
+                        </div>
+                        <div class="item-price">₹1000</div>
+                    </div>
+
+                    <div class="menu-item">
+                        <div class="item-name">
+                            <h3>Strawberry Sweet Cake</h3>
+                            <p>Chocolate pudding, vanilla, fruit raspberry jam milk</p>
+                        </div>
+                        <div class="item-price">₹1000</div>
+                    </div>
+                </div>
+                <div class="col-6">
+
+
+                    <div class="menu-item">
+                        <div class="item-name">
+                            <h3>Fried Egg Sandwich</h3>
+                            <p>Chocolate pudding, vanilla, fruit raspberry jam milk</p>
+                        </div>
+                        <div class="item-price">₹1000</div>
+                    </div>
+
+                    <div class="menu-item">
+                        <div class="item-name">
+                            <h3>Multigrain Hot Cake</h3>
+                            <p>Chocolate pudding, vanilla, fruit raspberry jam milk</p>
+                        </div>
+                        <div class="item-price">₹1000</div>
+                    </div>
+
+                    <div class="menu-item">
+                        <div class="item-name">
+                            <h3>Branch Special Cake</h3>
+                            <p>Chocolate pudding, vanilla, fruit raspberry jam milk</p>
+                        </div>
+                        <div class="item-price">₹1000</div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+
+
+
+    </div>
+    <script>
+        const slides = [
+            { image: "bootstrap/images/home-img.png", title: "Freshly baked pastries are the true <br><strong>language of love</strong>", text: "This drastic appeal for baked goods promoted baking all throughout Europe and expanded into the eastern parts of Asia." },
+            { image: "bootstrap/images/home-img.png", title: "Indulge in the Sweetness of Life <br><strong>language of love</strong>", text: "Every bite of our handcrafted cakes brings a moment of joy and delight." },
+            { image: "bootstrap/images/home-img.png", title: "A Slice of Happiness in Every Bite <br><strong>language of love</strong>", text: "Our cakes are made with love, perfect for every occasion and celebration." }
+        ];
+        let currentSlide = 0;
+        function updateSlide() {
+            document.getElementById("hero-img").src = slides[currentSlide].image;
+            document.getElementById("hero-title").innerHTML = slides[currentSlide].title;
+            document.getElementById("hero-text").innerHTML = slides[currentSlide].text;
+        }
+        function nextSlide() {
+            currentSlide = (currentSlide + 1) % slides.length;
+            updateSlide();
+        }
+        function prevSlide() {
+            currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+            updateSlide();
+        }
+        const containerdiv = document.getElementById("featured-cakes");
+
+        function scrollLeftDest() {
+            containerdiv.scrollBy({ left: -300, behavior: "smooth" });
+        }
+
+        function scrollRightDest() {
+            containerdiv.scrollBy({ left: 300, behavior: "smooth" });
+        }
+    </script>
+    <script src="bootstrap/js/slime.js"></script>
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
